7a04106800483dfe0ef57ba49f6fe8bc07bd5ab2
Tomás Santos <tomassantos314@gmail.com>
2025-01-07 16:29:45 +0000

    cards

---
+++ uxgenie/src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+    <div className="room">
+      <div className="home">
+        
+      </div>
+    </div>
+    <div className='card'>
+    <h2>Home Page</h2>
+    </div>
+    <div className="card-secundary">
+      <div className="secundary-overlay">
+      </div>
+      <div className="circle">24</div>
+    </div>
+   
+    
+    </>
+
+  );
+}
+
+export default Home;
