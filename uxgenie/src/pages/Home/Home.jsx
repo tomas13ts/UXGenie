3e1fad21a0d4dcc0a6ca19e351270770a21f384a
Tomás Santos <tomassantos314@gmail.com>
2025-01-07 17:29:13 +0000

    Header with icons

--- uxgenie/src/pages/Home/Home.jsx
+++ uxgenie/src/pages/Home/Home.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import './Home.css';
+import Header from '../../components/Header/Header';
 
 function Home() {
   return (
     <>
+    <Header/>
     <div className="room">
       <div className="home">
         
       </div>
     </div>
-    <div className='card'>
-    <h2>Home Page</h2>
-    </div>
-    <div className="card-secundary">
-      <div className="secundary-overlay">
-      </div>
-      <div className="circle">24</div>
-    </div>
+  
    
     
     </>
